54f70bc8b605bd0da9f3a8227f265cd320e46122
Hatsune Miku <73402249+Weebed-Coder@users.noreply.github.com>
2023-01-30 07:23:11 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-from flask import Flask, render_template, request
-import encryption
+from flask import Flask, render_template, request, make_response, redirect
+import hashlib
 
 app = Flask(__name__, static_url_path='/static')
 
 def check_auth(password:str=None):
-    print(password)
-    if password is None or password.strip() is "":
-        return "Password not supplied."
-    with open("auth_key.txt", "r", encoding="utf-8") as auth_key:
-        auth_key = auth_key.read()
-        with open("password_encrypted.txt", "r", encoding="utf-8") as passe:
-            passe = passe.read()
-            if password != encryption.decrypt(passe, int(auth_key)):
-                return "Incorrect password"
-            return True
+    if password is None or password.strip() == "":
+        return "Password not provided."
+    with open('password_hash.txt', 'r', encoding='utf-8') as password_hashe:
+        password_hash = password_hashe.read()
+        result = hashlib.sha512(password.encode())
+        if result.hexdigest() not in password_hash:
+            return "Invalid password."
+        return True
+
+def add_password(password:str=None):
+    if password is None:
+        return "Password is not provided."
+    with open('password_hash.txt', 'a', encoding="utf-8") as password_hash:
+        result = hashlib.sha512(password.encode())
+        password_hash.write(f"\n{result.hexdigest()}")
+        return "Password added successfully."
 
 @app.route("/")
-def home():
-    return render_template("auth.html", additional="")
+def index():
+    cookie = request.cookies.get("password")
+    if cookie is None:
+        return render_template("auth.html", additional="")
+    result = check_auth(cookie)
+    if result is not True:
+        return render_template("auth.html", additional=result)
+    return redirect("/index")
 
 @app.route('/', methods=['POST'])
 def index_post():
@@ -25,7 +37,29 @@ def index_post():
     result = check_auth(text)
     if result is not True:
         return render_template("auth.html", additional=result)
-    return render_template("index.html")
-    
+    resp = make_response(redirect("/index"))
+    resp.set_cookie('password', text)
+    return resp
+
+@app.route('/index')
+def command_line():
+    cookie = request.cookies.get("password")
+    result = check_auth(cookie)
+    if result is not True:
+        return redirect("/")
+    return render_template('index.html')
+
+@app.route('/index', methods=['POST'])
+def command_line_post():
+    cmd = request.form['cmd']
+    cookie = request.cookies.get("password")
+    result = check_auth(cookie)
+    if result is not True:
+        return redirect("/")
+    if cmd.strip() == "":
+        return render_template('index.html')
+    res = eval(cmd)
+    return render_template('index.html', result=res)
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
